2a1b6d3f4135e926734a43c56890633ddc2b8ea3
franedsandi <franedsandi@gmail.com>
2023-10-25 18:02:25 +0200

    [feat] first results

--- censura.php
+++ censura.php
@@ -1,5 +1,8 @@
 <?php
-
+  $paragraph = $_POST['paragraph'];
+  $lengthParagraph = strlen($paragraph);
+  $word = $_POST['word'];
+  $paragrafWithWord = $_POST['paragraph'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,6 +12,8 @@
   <title>PHP badwords</title>
 </head>
 <body>
-  
+<h1>Risultato</h1>
+  <h2>il paragrafo è "<?php echo $paragraph ?>";</h2>
+  <h2>il paragrafo contiene: <?php echo $lengthParagraph ?> lettere.</h2>
 </body>
 </html>
