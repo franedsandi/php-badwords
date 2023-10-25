e5afe995d3922eef5f0d2d88b8e2e0876c7563b8
franedsandi <franedsandi@gmail.com>
2023-10-25 18:04:31 +0200

    [feat] censorship

--- censura.php
+++ censura.php
@@ -3,6 +3,11 @@
   $lengthParagraph = strlen($paragraph);
   $word = $_POST['word'];
   $paragrafWithWord = $_POST['paragraph'];
+
+  if (str_contains($paragrafWithWord , $word)) {
+    $paragrafWithWord  = str_replace($word, '***', $paragrafWithWord);
+    $lengthParagraphWord = strlen($paragrafWithWord);
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,5 +20,8 @@
 <h1>Risultato</h1>
   <h2>il paragrafo è "<?php echo $paragraph ?>";</h2>
   <h2>il paragrafo contiene: <?php echo $lengthParagraph ?> lettere.</h2>
+
+  <h2>il nuovo paragrafo è "<?php echo $paragrafWithWord ?>";</h2>
+  <h2>il nuovo paragrafo contiene: <?php echo $lengthParagraphWord ?> lettere.</h2>
 </body>
 </html>
